12daa2a15671803ad3e734aeccf2d9cd22c82615
admin <admin@DESKTOP-1KSBD9G>
2023-11-02 15:14:08 +0530

    Created new class dec04

---
+++ src/gethub22/dec04.java
@@ -0,0 +1,17 @@
+package gethub22;
+
+public class dec04 {
+
+	public static void main(String[] args) {
+
+		int a=2;
+		int b=3;
+		
+		for(int i=1;i<=10;i++) {
+			System.out.print(a+"*"+i+"="+a*i);
+			System.out.print("   ");
+			System.out.println(b+"*"+i+"="+b*i);
+		}
+	}
+
+}
