109dbaab4b7c0b6c8629930e451fd5d55d81b997
suryavamsi <suryavamsi@localhost>
2023-11-02 16:00:04 +0530

    updated

--- src/gethub22/surya.java
+++ src/gethub22/surya.java
@@ -3,6 +3,27 @@ package gethub22;
 public class surya {
 	public static void main(String[] args) {
 		System.out.println("surya");
+		int b=20;
+		for(int a=9;a<=25;a++) {
+			
+			System.out.println(a);
+			if(a>b){
+				System.out.println("a is greaterthan b");
+			}
+			
+			else if(a==b) {
+				System.out.println("a is equal to b");
+				
+			}
+			
+			else {
+				System.out.println("a is lessthan b");
+			}
+			
+		}
+
 	}
 
-}
+	}
+
+
