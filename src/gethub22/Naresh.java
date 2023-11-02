6f05439e8ed0b881a02e04ad73284cf1233d6a95
Dell <Dell@DESKTOP-Q6G2HHN>
2023-11-02 16:24:01 +0530

    updated new class

---
+++ src/gethub22/Naresh.java
@@ -0,0 +1,15 @@
+package gethub22;
+
+public class Naresh {
+
+	public static void main(String[] args) {
+
+		for (int i =10 ; i <= 10; i++) {
+			for (int j = 10; j >0; j--) {
+				
+				System.out.println(i+"*"+j+"="+ i*j);
+			}
+			}
+	}
+
+}
